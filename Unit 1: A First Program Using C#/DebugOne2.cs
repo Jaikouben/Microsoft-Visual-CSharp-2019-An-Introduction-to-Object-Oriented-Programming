73c40ec6f07d74debd340bb2fd5f0b2b956b55e6
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:38:39 -0500

    Create DebugOne2.cs

---
+++ Unit 1: A First Program Using C#/DebugOne2.cs
@@ -0,0 +1,17 @@
+/*
+The provided file has syntax and/or logical errors. Determine the problem and fix the program.
+*/
+
+using static System.Console;
+class DebugOne2
+{
+   static void Main()
+   {
+      WriteLine("This program displays a square");
+      WriteLine("&&&&&&&&&&");
+      WriteLine("&        &");
+      WriteLine("&        &");
+      WriteLine("&        &");
+      WriteLine("&&&&&&&&&&");
+   }
+}
