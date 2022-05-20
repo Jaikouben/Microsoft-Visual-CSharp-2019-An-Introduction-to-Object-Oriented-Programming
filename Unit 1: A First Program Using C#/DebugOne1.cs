0271de8371828bebd1e53405f6e68b9475ef70db
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:37:38 -0500

    Create DebugOne1.cs

---
+++ Unit 1: A First Program Using C#/DebugOne1.cs
@@ -0,0 +1,16 @@
+/*
+The provided file has syntax and/or logical errors. Determine the problem and fix the program.
+*/
+
+using static System.Console;
+class DebugOne1
+{
+   static void Main()
+   {
+      WriteLine("This program displays some keyboard punctuation");
+      WriteLine("!   exclamation point");
+      WriteLine("@   at-sign");
+      WriteLine("#   pound sign or hash mark");
+      WriteLine("$   dollar sign");
+   }
+}
