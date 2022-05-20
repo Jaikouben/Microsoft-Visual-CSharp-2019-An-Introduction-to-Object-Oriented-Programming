324ac0c54e8c74ced445670ee0ca7c4d30cf7c0e
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:21:21 -0500

    Create PersonalInfo.cs

---
+++ Unit 1: A First Program Using C#/PersonalInfo.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+class PersonalInfo
+{
+   static void Main()
+   {
+      // Print name, birthdate, work phone number, and cell phone number
+      WriteLine("Jaikouben");
+      WriteLine("5/11/2004");
+      WriteLine("Work 000-000-0000");
+      WriteLine("Cell 000-000-0000");
+   }
+}
