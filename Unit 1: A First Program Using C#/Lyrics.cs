ed65d3147a54dac8162cc39e76f2f39aa17062c8
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:24:50 -0500

    Create Lyrics.cs

---
+++ Unit 1: A First Program Using C#/Lyrics.cs
@@ -0,0 +1,12 @@
+using static System.Console;
+class Lyrics
+{
+  static void Main()
+  {
+    // Displays the first four lines of my favorite song
+    WriteLine("We're no strangers to love");
+    WriteLine("You know the rules and so do I");
+    WriteLine("A full commitment's what I'm thinking of");
+    WriteLine("You wouldn't get this from any other guy");
+  }
+}
