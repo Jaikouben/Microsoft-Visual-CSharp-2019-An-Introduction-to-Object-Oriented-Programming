e367889303807637857b53ee1a3bb61ff8226410
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:29:06 -0500

    Update PersonalInfo.cs

--- Unit 1: A First Program Using C#/PersonalInfo.cs
+++ Unit 1: A First Program Using C#/PersonalInfo.cs
@@ -1,3 +1,16 @@
+/*
+
+Write, compile, and test a program named PersonalInfo that displays a person’s name, 
+birthdate, work phone number, and cell phone number on separate lines. 
+Follow the format in the example below:
+
+Lucinda Potter
+6/24/1992
+work 000-555-0101
+cell 000-555-0189
+
+*/
+
 using static System.Console;
 class PersonalInfo
 {
