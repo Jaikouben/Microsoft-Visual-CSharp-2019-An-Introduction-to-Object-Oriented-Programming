a701408704ec890e434498f877451ccffc165127
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:45:02 -0500

    Create MarshallsMotto.cs

---
+++ Unit 1: A First Program Using C#/MarshallsMotto.cs
@@ -0,0 +1,28 @@
+/*
+The case problems in this section introduce two fictional businesses. 
+You will create increasingly complex classes for these businesses that use the newest concepts you have mastered in each chapter.
+
+Instructions
+Marshall’s Murals is a company that paints interior and exterior murals for both business and residential customers.
+
+Write a program named MarshallsMotto that displays the company motto, which is “Make your vision your view.” 
+Edit the program so that it displays the motto surrounded by a border composed of repeated M s.
+
+Your output should like the following:
+
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+M  Make your vision your view.  M
+MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+*/
+
+using static System.Console;
+class MarshallsMotto
+{
+   static void Main()
+   {
+    // Display Marshalls motto
+    WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+    WriteLine("M  Make your vision your view.  M");
+    WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+   }
+}
