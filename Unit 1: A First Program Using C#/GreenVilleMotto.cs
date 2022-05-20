3da86932199c6a023bbe2a21d2169fba8feed686
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:42:43 -0500

    Create GreenVilleMotto.cs

---
+++ Unit 1: A First Program Using C#/GreenVilleMotto.cs
@@ -0,0 +1,29 @@
+/*
+The case problems in this section introduce two fictional businesses. 
+You will create increasingly complex classes for these businesses that use the newest concepts you have mastered in each chapter.
+
+Instructions
+Greenville County hosts the Greenville Idol competition each summer during the county fair. 
+The talent competition takes place over a three-day period during which contestants are eliminated 
+following rounds of performances until the year’s ultimate winner is chosen.
+
+Write a program named GreenvilleMotto that displays the competition’s motto, which is “The stars shine in Greenville.” 
+Edit the program so that it displays the motto surrounded by a border composed of asterisks.
+
+Your output should look like the following:
+************************************
+*  The stars shine in Greenville.  *
+************************************
+*/
+
+using static System.Console;
+class GreenvilleMotto
+{
+   static void Main()
+   {
+    // Display the Greenville motto
+    WriteLine("************************************");
+    WriteLine("*  The stars shine in Greenville.  *");
+    WriteLine("************************************");
+   }
+}
