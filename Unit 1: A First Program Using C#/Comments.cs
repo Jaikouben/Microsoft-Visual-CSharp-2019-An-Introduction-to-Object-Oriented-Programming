6ddd900fda89eff016d9bc2002b51ba2cc5c88d0
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:26:42 -0500

    Create Comments.cs

---
+++ Unit 1: A First Program Using C#/Comments.cs
@@ -0,0 +1,17 @@
+/*
+Write a program named Comments that displays a statement that defines program comments. 
+Include at least one block comment, one line comment, and one WriteLine() statement in the program.
+*/
+
+using static System.Console;
+class Comments
+{
+   static void Main()
+   {
+      WriteLine("Comments are useful things for forgetful people");
+      // This is a line comment
+      /*
+      This is a block comment
+      */
+   }
+}
