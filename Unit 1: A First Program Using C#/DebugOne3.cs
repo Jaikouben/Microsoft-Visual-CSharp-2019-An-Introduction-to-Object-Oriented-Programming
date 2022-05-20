054adc839a412a3ce102604dc7f76d84a86bdef4
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:39:30 -0500

    Create DebugOne3.cs

---
+++ Unit 1: A First Program Using C#/DebugOne3.cs
@@ -0,0 +1,17 @@
+/*
+The provided file has syntax and/or logical errors. Determine the problem and fix the program.
+*/
+
+using static System.Console;
+class DebugOne3
+{
+   static void Main()
+   {
+      WriteLine("This program lists the number 1 to 5 vertically");
+      WriteLine("1");
+      WriteLine("2");
+      WriteLine("3");
+      WriteLine("4");
+      WriteLine("5");
+   }
+}
