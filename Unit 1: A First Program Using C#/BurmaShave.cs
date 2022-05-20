64df2affdd9056e193a180e03654f12d95b80a28
Jaikouben <59106321+Jaikouben@users.noreply.github.com>
2022-05-20 15:36:24 -0500

    Create BurmaShave.cs

---
+++ Unit 1: A First Program Using C#/BurmaShave.cs
@@ -0,0 +1,28 @@
+/*
+From 1925 through 1963, Burma Shave advertising signs appeared next to highways all across the United States. 
+There were always four or five signs in a row containing pieces of a rhyme, followed by a final sign that read “Burma Shave.” 
+For example, one set of signs that has been preserved by the Smithsonian Institution reads as follows:
+
+Shaving brushes
+You’ll soon see ’em
+On a shelf
+In some museum
+Burma Shave
+
+Instructions
+Find a classic Burma Shave rhyme on the Web and write a program named BurmaShave that displays the rhyme.
+*/
+
+using static System.Console;
+class BurmaShave
+{
+   static void Main()
+   {
+    // Display Burma Shave rhymes
+    WriteLine("Shaving brushes");
+    WriteLine("You'll soon see 'em");
+    WriteLine("On a shelf");
+    WriteLine("In some museum");
+    WriteLine("Burma Shave");
+   }
+}
